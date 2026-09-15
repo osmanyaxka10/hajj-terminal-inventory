1d6ce8a55886177ce440faa5247bf99968143f40
osmanyaxka10 <osmanyaxka10@gmail.com>
2026-09-15 04:29:16 +0300

    Add simple stock status colours

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="hajj-terminal-v13";
+const CACHE="hajj-terminal-v14";
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["/","/manifest.webmanifest","/icon.svg"])));
   self.skipWaiting();
