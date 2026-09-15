dd151fc4526d6eef992149d80e3c379ba5ad4a6f
osmanyaxka10 <osmanyaxka10@gmail.com>
2026-09-15 04:22:41 +0300

    Expand Hajj Terminal product catalogue

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="hajj-terminal-v11";
+const CACHE="hajj-terminal-v12";
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["/","/manifest.webmanifest","/icon.svg"])));
   self.skipWaiting();
