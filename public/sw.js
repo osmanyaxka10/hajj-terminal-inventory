074ee627d506efae289d7f8709ee6db698776917
osmanyaxka10 <osmanyaxka10@gmail.com>
2026-09-15 04:26:43 +0300

    Limit catalogue to Hajj Terminal products

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="hajj-terminal-v12";
+const CACHE="hajj-terminal-v13";
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["/","/manifest.webmanifest","/icon.svg"])));
   self.skipWaiting();
