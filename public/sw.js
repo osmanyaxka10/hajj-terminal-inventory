514e3abaaf399db3d2565ab231e4a31a87e4c4e1
osmanyaxka10 <osmanyaxka10@gmail.com>
2026-09-15 03:46:24 +0300

    Start Hajj inventory with a clean cloud ledger

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="hajj-terminal-v8";
+const CACHE="hajj-terminal-v9";
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["/","/manifest.webmanifest","/icon.svg"])));
   self.skipWaiting();
