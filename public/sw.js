e8f03b29a4323b7c2ca911eb0f0adea27b1683da
osmanyaxka10 <osmanyaxka10@gmail.com>
2026-09-15 03:41:14 +0300

    Refresh PWA cache for weekly forecast

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="hajj-terminal-v7";
+const CACHE="hajj-terminal-v8";
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["/","/manifest.webmanifest","/icon.svg"])));
   self.skipWaiting();
