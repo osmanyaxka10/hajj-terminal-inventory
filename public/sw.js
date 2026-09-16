bf4ed34df9da1728d1220b20c818a3bea43c7554
osmanyaxka10 <osmanyaxka10@gmail.com>
2026-09-16 10:25:27 +0300

    Prioritize daily weekly monthly sales reports

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="hajj-terminal-v26";
+const CACHE="hajj-terminal-v27";
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["/","/count","/manifest.webmanifest","/icon.svg"])));
   self.skipWaiting();
