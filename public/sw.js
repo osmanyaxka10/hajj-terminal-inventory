5775a66b7ff608454e1c35e12a611579edfa8b44
osmanyaxka10 <osmanyaxka10@gmail.com>
2026-09-16 05:10:41 +0300

    Fix phone stock workflow and built-in assistant

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="hajj-terminal-v20";
+const CACHE="hajj-terminal-v21";
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["/","/manifest.webmanifest","/icon.svg"])));
   self.skipWaiting();
